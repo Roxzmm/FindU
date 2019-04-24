028b1af04be6fb451a1a6038a18b1ad2eec0d9ef
JingZhangFei <J.Zhang147@student.liverpool.ac.uk>
2019-04-24 16:31:50 +0100

    updated  mapView

--- FindU/SearchFacilityViewController.swift
+++ FindU/SearchFacilityViewController.swift
@@ -60,14 +60,449 @@ class SearchFacilityViewController: UIViewController,MKMapViewDelegate,CLLocatio
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+// mapView
+    
+//    import UIKit
+//    import MapKit
+//    import CoreLocation
+//    import CoreData
+//
+//    class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate, CLLocationManagerDelegate{
+//
+//
+//
+//        var locationManager = CLLocationManager()
+//
+//        let userLoc = CLLocation(latitude:CLLocationDegrees(53.406566), longitude: CLLocationDegrees(-2.966531))
+//
+//        var locationBydistance : [String] = []
+//        var titleN = ""
+//        var artistN = ""
+//        var yearOfWorkN = ""
+//        var informationN = ""
+//
+//        @IBOutlet weak var map: MKMapView!
+//
+//        @IBOutlet weak var table: UITableView!
+//
+//
+//
+//
+//        var artInformation1: [ArtworkInformation] = []
+//        var sectionByLocation : [String : [ArtworkInformation]] = [:]
+//
+//
+//
+//        override func viewDidLoad() {
+//            super.viewDidLoad()
+//
+//
+//            locationManager.delegate = self as CLLocationManagerDelegate
+//            locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+//            locationManager.requestWhenInUseAuthorization()
+//            locationManager.startUpdatingLocation()
+//
+//
+//            //第一次启动
+//            if (!(UserDefaults.standard.bool(forKey: "everLaunched"))) {
+//                UserDefaults.standard.set(true, forKey:"everLaunched")
+//                let firstOpenViewController = FirstOpenViewController()
+//                firstOpenViewController.jsonSave()
+//
+//                let date = Date()
+//                let formatter = DateFormatter()
+//                formatter.dateFormat = "yyyy-MM-dd"
+//                let todayString = formatter.string(from: date)
+//                UserDefaults.standard.set(todayString, forKey: "todayStringKey")
+//
+//
+//
+//
+//            }
+//            else{
+//                //除第一次登陆以外的情况
+//
+//                jsonSave()
+//                fetchInf()
+//                Arraybylocation()
+//                self.table.reloadData()
+//
+//                //            let RetrivedDate = UserDefaults.standard.object(forKey: "todayStringKey") as? String
+//                //            print(RetrivedDate)
+//            }
+//            //
+//            //         table.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+//        }
+//
+//        func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//
+//            let lable = UILabel()
+//            lable.text = locationBydistance[section]
+//            lable.backgroundColor = UIColor.lightGray
+//            return lable
+//
+//        }
+//
+//        func numberOfSections(in tableView: UITableView) -> Int {
+//            return locationBydistance.count
+//        }
+//        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//            return sectionByLocation[locationBydistance[section]]!.count
+//        }
+//
+//        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//            let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "myCell")
+//            let title = sectionByLocation[locationBydistance[indexPath.section]]![indexPath.row].title
+//            let author = sectionByLocation[locationBydistance[indexPath.section]]![indexPath.row].artist
+//
+//            cell.textLabel?.text = title
+//            cell.detailTextLabel?.text = author
+//            return cell
+//        }
+//
+//        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//            tableView.deselectRow(at: indexPath, animated: true)
+//
+//
+//            titleN = sectionByLocation[locationBydistance[indexPath.section]]![indexPath.row].title!
+//            artistN = sectionByLocation[locationBydistance[indexPath.section]]![indexPath.row].artist!
+//            informationN = sectionByLocation[locationBydistance[indexPath.section]]![indexPath.row].information!
+//            yearOfWorkN = sectionByLocation[locationBydistance[indexPath.section]]![indexPath.row].yearOfWork!
+//
+//
+//            performSegue(withIdentifier: "showDetail", sender: self)
+//
+//
+//        }
+//        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            let des = segue.destination as! detailView
+//            des.titleName = titleN
+//            des.artistName =  artistN
+//            des.informationName = informationN
+//            des.yearOfWorkName =  yearOfWorkN
+//
+//            //        favourite = des.returnFavourite()
+//            //        updateFavourite(title: titleN,favourite: favourite)
+//
+//
+//        }
+//
+//
+//
+//
+//        override func viewWillAppear(_ animated: Bool) {
+//        }
+//
+//        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//
+//            let locationOfUser = locations[0]
+//
+//            //            userLoc = locations[0]
+//            let latitude = locationOfUser.coordinate.latitude
+//            let longitude = locationOfUser.coordinate.longitude
+//            let latDelta: CLLocationDegrees = 0.0005
+//            let lonDelta: CLLocationDegrees = 0.0005
+//            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+//            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//            let region = MKCoordinateRegion(center: location, span: span)
+//            self.map.setRegion(region, animated: true)
+//
+//            //            Arraybylocation()
+//        }
+//
+//
+//        func jsonSave(){
+//            struct artInfor: Decodable {
+//
+//                let id: String?
+//                let title: String?
+//                let artist: String?
+//                let yearOfWork: String?
+//                let information: String?
+//                let lat: String?
+//                let long: String?
+//                let location: String?
+//                let locationNotes: String?
+//                let fileName: String?
+//                let lastModified: String?
+//                let enabled: String?
+//
+//            }
+//
+//            struct artInformation: Decodable {
+//                let campus_artworks: [artInfor]
+//            }
+//            let RetrivedDate = UserDefaults.standard.object(forKey: "todayStringKey") as? String
+//
+//
+//
+//            if let url = URL(string: "https://cgi.csc.liv.ac.uk/~phil/Teaching/COMP228/artworksOnCampus/data.php?class=campus_artworks&lastUpdate=\(RetrivedDate!)"){
+//                //2019-02-20
+//                let session = URLSession.shared
+//
+//                //if 没有更新
+//                session.dataTask(with:url) { (data, response, err) in
+//
+//                    guard let jsonData = data else {
+//                        return }
+//
+//                    do{
+//                        let decoder = JSONDecoder()
+//                        let artList = try decoder.decode(artInformation.self, from: jsonData)
+//
+//                        //                    if self.artInformation1.count != artList.artInfor2.count{
+//                        for OInfor in self.artInformation1{
+//                            for aInfor in artList.campus_artworks {
+//                                if OInfor.id == aInfor.id{
+//                                    DispatchQueue.main.async {
+//                                        self.update(id: OInfor.id,title:OInfor.title,artist: OInfor.artist,yearOfWork: OInfor.yearOfWork,information: OInfor.information,lat: OInfor.lat,long: OInfor.long,location: OInfor.location,locationNotes: OInfor.locationNotes,fileName: OInfor.fileName,lastmodified:OInfor.lastModified,enabled:OInfor.enabled )
+//                                        print("update:\(OInfor.id!)")
+//                                        self.fetchInf()
+//                                        self.table.reloadData()
+//                                    }
+//                                }
+//                                else{
+//
+//                                    var count = 0
+//                                    for TInfor in self.artInformation1{
+//                                        if TInfor.id != aInfor.id{
+//                                            count = count + 1
+//                                        }
+//                                    }
+//                                    if count == self.artInformation1.count{
+//                                        DispatchQueue.main.async {
+//                                            self.save(id: aInfor.id,title:aInfor.title,artist: aInfor.artist,yearOfWork: aInfor.yearOfWork,information: aInfor.information,lat: aInfor.lat,long: aInfor.long,location: aInfor.location,locationNotes: aInfor.locationNotes,fileName: aInfor.fileName,lastmodified:aInfor.lastModified,enabled:aInfor.enabled)
+//                                            print("add:\(OInfor.id!)")
+//                                            self.fetchInf()
+//                                            self.table.reloadData()
+//                                        }
+//                                    }
+//
+//
+//                                }
+//
+//                            }
+//                        }
+//
+//
+//
+//                    } catch let jsonErr {
+//
+//                        print("Error decoding JSON", jsonErr)
+//                    }
+//                    }.resume()
+//            }
+//            let date = Date()
+//            let formatter = DateFormatter()
+//            formatter.dateFormat = "yyyy-MM-dd"
+//            let todayString = formatter.string(from: date)
+//            UserDefaults.standard.set(todayString, forKey: "todayStringKey")
+//
+//        }
+//
+//        func save(id: String?, title: String?, artist: String?, yearOfWork: String?,information: String?,lat: String? ,long: String? , location: String?,locationNotes: String?,fileName: String?, lastmodified: String?,enabled: String?) {
+//
+//            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+//                return
+//            }
+//
+//            let managedContext = appDelegate.persistentContainer.viewContext
+//
+//            let entity = NSEntityDescription.entity(forEntityName: "ArtworkInformation", in: managedContext)!
+//            let ArtworkInformation = NSManagedObject(entity: entity, insertInto: managedContext)
+//            ArtworkInformation.setValue(id, forKeyPath: "id")
+//            ArtworkInformation.setValue(title, forKeyPath: "title")
+//            ArtworkInformation.setValue(artist, forKeyPath: "artist")
+//            ArtworkInformation.setValue(yearOfWork, forKeyPath: "yearOfWork")
+//            ArtworkInformation.setValue(information, forKeyPath: "information")
+//            ArtworkInformation.setValue(lat, forKeyPath: "lat")
+//            ArtworkInformation.setValue(long, forKeyPath: "long")
+//            ArtworkInformation.setValue(location, forKeyPath: "location")
+//            ArtworkInformation.setValue(locationNotes, forKeyPath: "locationNotes")
+//            ArtworkInformation.setValue(fileName, forKeyPath: "fileName")
+//            ArtworkInformation.setValue(lastmodified, forKey: "lastModified")
+//            ArtworkInformation.setValue(enabled, forKey: "enabled")
+//
+//
+//
+//
+//            do {
+//                try managedContext.save()
+//
+//
+//            } catch let error as NSError {
+//                print("Could not save. \(error), \(error.userInfo)")
+//            }
+//        }
+//
+//        func update(id: String?, title: String?, artist: String?, yearOfWork: String?,information: String?,lat: String? ,long: String? , location: String?,locationNotes: String?,fileName: String?, lastmodified: String?,enabled: String?) {
+//
+//            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+//                return
+//            }
+//
+//            let managedContext = appDelegate.persistentContainer.viewContext
+//
+//            let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ArtworkInformation")
+//            fetchRequest.predicate = NSPredicate(format: "id == %@", id!)
+//            do {
+//                let result = try managedContext.fetch(fetchRequest) as! [ArtworkInformation]
+//                for infor in result {
+//                    infor.id = id
+//                    infor.title = title
+//                    infor.artist = artist
+//                    infor.yearOfWork = yearOfWork
+//                    infor.information = information
+//                    infor.lat = lat
+//                    infor.long = long
+//                    infor.location = location
+//                    infor.locationNotes = locationNotes
+//                    infor.fileName = fileName
+//                    infor.lastModified = lastmodified
+//                    infor.enabled = enabled
+//                }
+//            } catch let error as NSError {
+//                print("Could not fetch. \(error), \(error.userInfo)")
+//            }
+//
+//        }
+//
+//
+//        func fetchInf(){
+//
+//            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+//                return
+//            }
+//
+//            let managedContext = appDelegate.persistentContainer.viewContext
+//            let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ArtworkInformation")
+//
+//            do {
+//                let a = fetchRequest
+//                a.sortDescriptors = [NSSortDescriptor.init(key: "location", ascending: true)]
+//                artInformation1 = try managedContext.fetch(a) as! [ArtworkInformation]
+//                //            for a in artInformation1{
+//                //                print(a.location!)
+//                //            }
+//            } catch let error as NSError {
+//                print("Could not fetch. \(error), \(error.userInfo)")
+//            }
+//        }
+//
+//        func Arraybylocation(){
+//
+//            for i in 0...artInformation1.count-1{
+//                let lat = artInformation1[i].lat
+//                let latitude = Double(lat!)
+//                let lon = artInformation1[i].long
+//                let longitude = Double(lon!)
+//
+//                //                let a = latitude!
+//                //                let b
+//                let coordinate = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+//                let annotation = MKPointAnnotation()
+//                annotation.coordinate = coordinate
+//                annotation.title = artInformation1[i].location //显示什么？
+//                self.map.addAnnotation(annotation)
+//
+//            }
+//
+//
+//            var locationlist : [String] = []
+//            //            var locationNoteList: [String] = []
+//            var distance : [Double] = []
+//            var locationAndDistance: [String : Double] = [String : Double]()
+//            var finalLocationlist : [String] = []
+//
+//            //         var locationlist : [String] = []
+//            var lat : [Double] = []
+//            var long : [Double] = []
+//
+//
+//
+//            var howManyArtWorksInLocation : [Int] = []
+//            var startlocation = artInformation1[0].location!
+//            //            var startlocationNote = artInformation1[0].locationNotes!
+//            let startlocationLat = (Double)(artInformation1[0].lat!)!
+//            let startlocationLong = (Double)(artInformation1[0].long!)!
+//            //         var distance : [Double] = []
+//            var a = 0
+//            var b = 0
+//
+//            locationlist.append(startlocation)
+//            //            locationNoteList.append(startlocationNote)
+//            lat.append(startlocationLat)
+//            long.append(startlocationLong)
+//            for aArt in artInformation1{
+//
+//                if aArt.location! == startlocation{
+//                    b = b+1
+//                }
+//                else{
+//                    howManyArtWorksInLocation.append(b)
+//                    b = 1
+//                    startlocation = aArt.location!
+//                    locationlist.append(aArt.location!)
+//                    lat.append((Double)(aArt.lat!)!)
+//                    long.append((Double)(aArt.long!)!)
+//
+//
+//                }
+//                a = a+1
+//            }
+//            howManyArtWorksInLocation.append(b)
+//
+//            for i in 0...locationlist.count-1{
+//
+//                let artLcLocation = CLLocation(latitude:CLLocationDegrees(lat[i]), longitude: CLLocationDegrees(long[i]))
+//                //            print(userLoc!)
+//
+//                let artToUser = artLcLocation.distance(from: userLoc)
+//                distance.append(artToUser)
+//            }
+//
+//
+//            for i in 0...distance.count-1{
+//                //            print("\(locationlist[i]) : \(distance[i])")
+//                locationAndDistance[locationlist[i]] = distance[i]
+//            }
+//
+//
+//            let result4 = locationAndDistance.sorted { (str1, str2) -> Bool in
+//                return str1.1 < str2.1
+//            }
+//
+//            for key in 0...result4.count-1{
+//                finalLocationlist.append(result4[key].key)
+//            }
+//            self.locationBydistance = finalLocationlist
+//
+//            for i in 0..<finalLocationlist.count{
+//                var eachLocation: [ArtworkInformation] = []
+//                for aArt in self.artInformation1{
+//                    if aArt.location! == finalLocationlist[i]  {
+//                        eachLocation.append(aArt)
+//                    }
+//                }
+//                self.sectionByLocation[finalLocationlist[i]] = eachLocation
+//            }
+//            //        for i in 0...finalLocationlist.count-1{
+//            //            print(finalLocationlist[i])
+//            //        }
+//            //        print(result4)
+//            //        for a in locationlist{
+//            //            distance = userLoc.distance(from: a)
+//            //                    }
+//
+//        }
+//
+//        private func mapView(mapView:MKMapView, didSelectAnnotationView view:MKAnnotationView){
+//
+//            print("didSelectAnnotationView")
+//
+//        }
+//
+//    }
 
 }
